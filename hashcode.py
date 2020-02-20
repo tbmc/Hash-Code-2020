a48c9e2b9e71b5aa10a0ec1292e6f183bc353be5
Thibault <tbmc33@gmail.com>
2020-02-20 21:17:43 +0100

    YOOOO

--- hashcode.py
+++ hashcode.py
@@ -18,10 +18,18 @@ def parse_input(input_path: str):
 
     content = content[2:]
     libraries = []
+    book_added = set()
 
     for i in range(library_number):
         book_number, days, book_by_day = map(int, content[i * 2].split(" "))
         books = list(map(int, content[i * 2 + 1].split(" ")))
+
+        # books_with_score = []
+        # for book in books:
+        #     if book not in book_added:
+        #         book_added.add(book)
+        #         books_with_score.append((book_scores[book], book))
+        # books_with_score = sorted(books_with_score, reverse=True)
         books_with_score = sorted([(book_scores[book], book) for book in books], reverse=True)
         scores, book_sorted = list(zip(*books_with_score))
 
@@ -85,14 +93,26 @@ def compute_simple(book_number: int, library_number: int, max_day_number: int, l
 
 
 def compute_stupid(book_number: int, library_number: int, max_day_number: int, libraries: List[Tuple[int, int, int, List[int], List[int]]], output: str):
+    computed_books = set()
     libs_output = {}
     for score, i, days, book_by_day, books, book_sorted in sorted(libraries, reverse=True):
-        libs_output[i] = book_sorted
+        book_filtered = []
+        for book in book_sorted:
+            if book not in computed_books:
+                computed_books.add(book)
+                book_filtered.append(book)
 
-    out = f"{len(libs_output)}"
+        libs_output[i] = book_filtered
+
+    out = ""  # f"{len(libs_output)}"
     for idx, books in libs_output.items():
+        if len(books) < 1:
+            continue
         out += f"\n{idx} {len(books)}\n"
         out += " ".join(map(str, books))
+
+    ll = len(out.split('\n'))
+    out = f"{int(ll / 2)}{out}"
 
     with open(output, "w") as f:
         f.write(out)
